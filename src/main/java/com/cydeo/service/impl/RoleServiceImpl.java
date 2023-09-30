6f5ad8b19bab84388a4134bef378cfbb385914bd
nazik7 <59424131+nazik7@users.noreply.github.com>
2023-09-30 06:16:38 -0500

    injected UserService and RoleService to DataGenerator and used @Service annotation instead of @Component in UserServiceImpl and RoleServiceImpl

--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -2,19 +2,21 @@ package com.cydeo.service.impl;
 
 import com.cydeo.dto.RoleDTO;
 import com.cydeo.service.RoleService;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class RoleServiceImpl extends AbstractMapService<RoleDTO, Long> implements RoleService{
 
     @Override
     public RoleDTO save(RoleDTO object) {
-        super.save(object.getId(),object);
+        return super.save(object.getId(),object);
     }
 
     @Override
     public List<RoleDTO> findAll() {
-        super.findAll();
+       return super.findAll();
     }
 
     @Override
